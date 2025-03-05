dbdb6844c5b48e6f65aa8d993f37d27b1997d6ca
zhourongchang399 <497447111@qq.com>
2025-03-05 15:57:09 +0800

    两数之和，字母异位词

---
+++ src/test/java/com/just/justleetcode/JustLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.just.justleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustLeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
