03a7ab7104a6de42d283fbacf26650a07de82a46
zhourongchang399 <497447111@qq.com>
2025-03-12 14:31:20 +0800

    缺失的第一个正数

---
+++ src/leetcode/editor/cn/[41]缺失的第一个正数.java
@@ -0,0 +1,68 @@
+//给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。 请你实现时间复杂度为 
+//O(n) 并且只使用常数级别额外空间的解决方案。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,0]
+//输出：3
+//解释：范围 [1,2] 中的数字都在数组中。 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,4,-1,1]
+//输出：2
+//解释：1 在数组中，但 2 没有。 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [7,8,9,11,12]
+//输出：1
+//解释：最小的正数 1 没有出现。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// 
+//
+// Related Topics 数组 哈希表 👍 2303 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @author: Zc
+ * @description: 数组缺失的第一个正数，nums.length 的数组缺失的第一个正数只会出现在 [1,N+1] 的范围内；
+ *               可以理解为当前数组中小于等于 nums.length && 大于等于 1 的值都在正确位置上时，遍历数组，
+ *               首先遇到不符合条件的位置即为缺失值，即 index + 1；
+ * @date: 2025/3/12 14:27
+ * @param null
+ * @return
+ */
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] >= 1 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
