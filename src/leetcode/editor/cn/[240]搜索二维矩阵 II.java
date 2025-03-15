36088155c979272fa75c752a665b68ce56c377d4
zhourongchang399 <497447111@qq.com>
2025-03-15 15:06:06 +0800

    搜索二维矩阵 Ⅱ

---
+++ src/leetcode/editor/cn/[240]搜索二维矩阵 II.java
@@ -0,0 +1,70 @@
+//编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性： 
+//
+// 
+// 每行的元素从左到右升序排列。 
+// 每列的元素从上到下升序排列。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21
+//,23,26,30]], target = 5
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21
+//,23,26,30]], target = 20
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= n, m <= 300 
+// -10⁹ <= matrix[i][j] <= 10⁹ 
+// 每行的所有元素从左到右升序排列 
+// 每列的所有元素从上到下升序排列 
+// -10⁹ <= target <= 10⁹ 
+// 
+//
+// Related Topics 数组 二分查找 分治 矩阵 👍 1621 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @author: Zc
+ * @description: 从右上角开始看就是二叉搜索树；
+ * @date: 2025/3/15 15:02
+ * @param null
+ * @return
+ */
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                j--;
+                continue;
+            }
+            if (matrix[i][j] < target) {
+                i++;
+                continue;
+            }
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
