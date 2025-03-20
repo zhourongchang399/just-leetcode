956115f78e3add10afedf31a81788c64a56d92fa
zhourongchang399 <497447111@qq.com>
2025-03-20 21:46:49 +0800

    K 个一组翻转链表

---
+++ src/leetcode/editor/cn/[25]K 个一组翻转链表.java
@@ -0,0 +1,91 @@
+//给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。 
+//
+// k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[2,1,4,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 3
+//输出：[3,2,1,4,5]
+// 
+//
+// 
+//提示：
+//
+// 
+// 链表中的节点数目为 n 
+// 1 <= k <= n <= 5000 
+// 0 <= Node.val <= 1000 
+// 
+//
+// 
+//
+// 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？ 
+//
+// 
+// 
+//
+// Related Topics 递归 链表 👍 2511 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * @author: Zc
+ * @description: 模拟递归，递归每一个子组并颠倒子组链表返回新的头节点，将下一个递归颠倒的子组的尾节点 next 即为上一个递归的头节点，最后返回新的头节点，以此往复得到答案；
+ * @date: 2025/3/20 21:44
+ * @param null
+ * @return
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        ListNode current = head;
+        int count = 0;
+        while (count < k && current != null) {
+            current = current.next;
+            count++;
+        }
+
+        if (count < k) {
+            return head;
+        }
+
+        ListNode nextHead = reverseKGroup(current, k);
+        ListNode currentHead = reverseLinedList(head, k);
+        head.next = nextHead;
+
+        return currentHead;
+    }
+
+    private ListNode reverseLinedList(ListNode head, int k) {
+        if (head == null || head.next == null || k == 1) return head;
+        ListNode nextNode = reverseLinedList(head.next, k - 1);
+        head.next.next = head;
+        head.next = null;
+        return nextNode;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
