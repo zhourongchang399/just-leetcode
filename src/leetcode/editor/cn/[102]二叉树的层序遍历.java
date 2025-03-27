3f0ebf69640feb9de5914d4557a423247432f521
zhourongchang399 <497447111@qq.com>
2025-03-27 13:38:40 +0800

    二叉树的层序遍历

--- src/leetcode/editor/cn/[102]二叉树的层序遍历.java
+++ src/leetcode/editor/cn/[102]二叉树的层序遍历.java
@@ -51,22 +51,53 @@
  *     }
  * }
  */
+/**
+ * @author: Zc
+ * @description: 1.递归分治，将二叉树的曾想遍历问题分解为一个个子问题，只需要求出左右子树的层序数组，再拼接二者即可得到当前节点的层序数组；
+ *               2.队列，将每一层的节点入队，节点出队时将其左右节点入队，每次记录每一层的数目，出队对应次数即存储为一层数组；
+ * @date: 2025/3/27 13:31
+ * @param null
+ * @return
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if(root == null) return new ArrayList<>();
-        List<List<Integer>> leftList = levelOrder(root.left);
-        List<List<Integer>> rightList = levelOrder(root.right);
-        for (int i = 0; i < rightList.size(); i++) {
-            if (i < leftList.size()) {
-                leftList.get(i).addAll(rightList.get(i));
-            } else {
-                leftList.add(i, rightList.get(i));
+//        if(root == null) return new ArrayList<>();
+//        List<List<Integer>> leftList = levelOrder(root.left);
+//        List<List<Integer>> rightList = levelOrder(root.right);
+//        for (int i = 0; i < rightList.size(); i++) {
+//            if (i < leftList.size()) {
+//                leftList.get(i).addAll(rightList.get(i));
+//            } else {
+//                leftList.add(i, rightList.get(i));
+//            }
+//        }
+//        List<Integer> current = new ArrayList<>();
+//        current.add(root.val);
+//        leftList.add(0, current);
+//        return leftList;
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> currentList = new ArrayList<>();
+            int currentSize = queue.size();
+            for (int i = 0; i < currentSize; i++) {
+                TreeNode node = queue.getFirst();
+                queue.removeFirst();
+                currentList.add(node.val);
+                if (node.left != null) {
+                    queue.addLast(node.left);
+                }
+                if (node.right != null) {
+                    queue.addLast(node.right);
+                }
             }
+            res.add(currentList);
         }
-        List<Integer> current = new ArrayList<>();
-        current.add(root.val);
-        leftList.add(0, current);
-        return leftList;
+        return res;
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
