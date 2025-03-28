1bfd4538ba6cba11615eafea56da0560b1b5ae37
zhourongchang399 <497447111@qq.com>
2025-03-28 12:01:19 +0800

    将有序数组转换为二叉搜索树

---
+++ src/leetcode/editor/cn/[108]将有序数组转换为二叉搜索树.java
@@ -0,0 +1,69 @@
+//给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 平衡 二叉搜索树。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：nums = [-10,-3,0,5,9]
+//输出：[0,-3,9,-10,null,5]
+//解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+//
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：nums = [1,3]
+//输出：[3,1]
+//解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums 按 严格递增 顺序排列 
+// 
+//
+// Related Topics 树 二叉搜索树 数组 分治 二叉树 👍 1634 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * @author: Zc
+ * @description: 二叉平衡树，只需要将问题分解，每个节点的左右子节点都是二叉平衡树，那整颗树就是二叉平衡树；
+ *               因为数组已经是升序的，所以只需将中间节点置为根节点，左边节点为左子树，右边节点为右子树，递归这个过程即可。
+ * @date: 2025/3/28 11:56
+ * @param null
+ * @return
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) return null;
+        int mid = nums.length/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, mid));
+        root.right = sortedArrayToBST(Arrays.copyOfRange(nums, mid + 1, nums.length));
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
