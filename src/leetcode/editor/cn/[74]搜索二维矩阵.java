82a75b79b1f147dc36068c888c2e41f08555af04
zhourongchang399 <497447111@qq.com>
2025-04-03 23:12:44 +0800

    搜索二维矩阵

---
+++ src/leetcode/editor/cn/[74]搜索二维矩阵.java
@@ -0,0 +1,64 @@
+//给你一个满足下述两条属性的 m x n 整数矩阵： 
+//
+// 
+// 每行中的整数从左到右按非严格递增顺序排列。 
+// 每行的第一个整数大于前一行的最后一个整数。 
+// 
+//
+// 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 100 
+// -10⁴ <= matrix[i][j], target <= 10⁴ 
+// 
+//
+// Related Topics 数组 二分查找 矩阵 👍 1038 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @author: Zc
+ * @description: 将一个非严格递增的二维数组看作一个从第一行最后一列为根节点的二叉搜索树即可。
+ * @date: 2025/4/3 23:08
+ * @param null
+ * @return
+ */
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
