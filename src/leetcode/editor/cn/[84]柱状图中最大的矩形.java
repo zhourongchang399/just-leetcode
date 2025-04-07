0a775781e927f1c17d72610044332720b4c59156
zhourongchang399 <497447111@qq.com>
2025-04-07 16:27:27 +0800

    柱状图中最大的矩形

--- src/leetcode/editor/cn/[84]柱状图中最大的矩形.java
+++ src/leetcode/editor/cn/[84]柱状图中最大的矩形.java
@@ -37,7 +37,14 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * @author: Zc
- * @description: 暴力解法
+ * @description: 寻找柱状图中最大的矩形，即寻找当前柱子的左右边界长度并乘当前柱子高度；通过遍历左右边界时间复杂度太高，但是通过观察可知道当左边柱子高度小于当前柱子高度，则他的最左边界最多到这个柱子，同理右边一样；
+ *               当左边柱子大于当前柱子高度则左边边界可能还能左扩直到遇到小于当前柱子高度的柱子，根据这个结论，我们可以通过单调递增栈记录左边柱子的高度，当当前柱子大于栈顶元素则左边界确定；
+ *               当当前柱子小于栈顶元素则出栈直到当前柱子大于栈顶元素，即可确定当前柱子的左边界，同理右边界。
+ *               结论可归纳为：
+ *               （1）当需要寻找当前位置左边第一个小于其值的位置时：从左到右遍历维护单调递增栈；
+ *               （2）当需要寻找当前位置右边第一个小于其值的位置时：从右到左遍历维护单调递增栈；
+ *               （3）当需要寻找当前位置左边第一个大于其值的位置时：从左到右遍历维护单调递减栈；
+ *               （4）当需要寻找当前位置右边第一个大于其值的位置时：从右到左遍历维护单调递减栈。
  * @date: 2025/4/7 14:19
  * @param null
  * @return
@@ -45,17 +52,30 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int res = Integer.MIN_VALUE;
+
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.add(-1);
+        int[] leftRange = new int[heights.length];
+        int[] rightRange = new int[heights.length];
         for (int i = 0; i < heights.length; i++) {
-            int h = heights[i];
-            int r = i + 1;
-            int l = i - 1;
-            while (l >= 0 && heights[l] >= h) {
-                l--;
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
             }
-            while (r < heights.length && heights[r] >= h) {
-                r++;
+            leftRange[i] = stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        stack.add(heights.length);
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (stack.peek() != heights.length && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
             }
-            res = Math.max(res, h * (r - (l + 1)));
+            rightRange[i] = stack.peek();
+            stack.push(i);
+        }
+
+        for (int i = 0; i < heights.length; i++) {
+            res = Math.max(res, heights[i] * (rightRange[i] - leftRange[i] - 1));
         }
         return res;
     }
