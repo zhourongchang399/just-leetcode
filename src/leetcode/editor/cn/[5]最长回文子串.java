88b976bb8344210be3cc7d693a1c525d71b09180
zhourongchang399 <497447111@qq.com>
2025-04-12 17:23:26 +0800

    最长回文子串

--- src/leetcode/editor/cn/[5]最长回文子串.java
+++ src/leetcode/editor/cn/[5]最长回文子串.java
@@ -32,30 +32,43 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * @author: Zc
- * @description: 暴力破解法
+ * @description: 中心拓展，如果当前位置 Si == Sj && P[i + 1, j - 1] is Palindrome 则 P[i, j] is Palindrome;
+ *               所以只需遍历字符串，分别对中心回文子串两边拓展直到不符合条件为止，并维护 startIndex 和 maxLength 即可；
+ *               但仍需考虑类似 abba 的情况，abba 是当前最长回文子串，但是无法通过当个位置开始向外扩找到答案；
+ *               所以需要补充一个拓展方式，即当 Si == Si+1 时，判断 Si-1 ?= Si+2,满足即拓展，不满足则结束。
  * @date: 2025/4/12 17:01
  * @param null
  * @return
  */
 class Solution {
     public String longestPalindrome(String s) {
-        for (int i = s.length(); i >= 1; i--) {
-            for (int j = 0; j + i <= s.length(); j++) {
-                if (myPalindrome(s, j, j + i - 1)) {
-                    return s.substring(j, j + i);
+        int maxLength = 0;
+        int maxStart = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) {
+                int start = i - 1, end = i + 2, currentLength = 2;
+                while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+                    currentLength += 2;
+                    start--;
+                    end++;
+                }
+                if (currentLength > maxLength) {
+                    maxStart = start + 1;
+                    maxLength = currentLength;
                 }
             }
-        }
-        return s.substring(0, 1);
-    }
-
-    public boolean myPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start++) != s.charAt(end--)) {
-                return false;
+            int start = i - 1, end = i + 1, currentLength = 1;
+            while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+                currentLength += 2;
+                start--;
+                end++;
+            }
+            if (currentLength > maxLength) {
+                maxStart = start + 1;
+                maxLength = currentLength;
             }
         }
-        return true;
+        return s.substring(maxStart, maxStart + maxLength);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
