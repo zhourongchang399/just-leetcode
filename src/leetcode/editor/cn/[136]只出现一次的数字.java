787277612a1fcb4b20989554acc9ea58c8f7755b
zhourongchang399 <497447111@qq.com>
2025-04-13 12:18:49 +0800

    只出现一次的数字

---
+++ src/leetcode/editor/cn/[136]只出现一次的数字.java
@@ -0,0 +1,65 @@
+//给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 
+//
+// 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。 
+//
+// 
+// 
+// 
+// 
+// 
+//
+// 示例 1 ： 
+//
+// 
+// 输入：nums = [2,2,1] 
+// 
+//
+// 输出：1 
+//
+// 示例 2 ： 
+//
+// 
+// 输入：nums = [4,1,2,1,2] 
+// 
+//
+// 输出：4 
+//
+// 示例 3 ： 
+//
+// 
+// 输入：nums = [1] 
+// 
+//
+// 输出：1 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 10⁴ 
+// -3 * 10⁴ <= nums[i] <= 3 * 10⁴ 
+// 除了某个元素只出现一次以外，其余每个元素均出现两次。 
+// 
+//
+// Related Topics 位运算 数组 👍 3295 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @author: Zc
+ * @description: 因为除了某个元素只出现一次以外，其余每个元素均出现两次，所以采用异或，两数相等异或为 0 ，任何数与 0 异或为任何数，同时异或满足交换律，所以答案为唯一值。
+ * @date: 2025/4/13 12:15
+ * @param null
+ * @return
+ */
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
