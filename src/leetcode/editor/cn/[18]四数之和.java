e3d8993d500392522e529fc78e067c383b565d3d
zhourongchang399 <497447111@qq.com>
2025-04-20 00:09:12 +0800

    四数之和

---
+++ src/leetcode/editor/cn/[18]四数之和.java
@@ -0,0 +1,87 @@
+//给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[
+//b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）： 
+//
+// 
+// 0 <= a, b, c, d < n 
+// a、b、c 和 d 互不相同 
+// nums[a] + nums[b] + nums[c] + nums[d] == target 
+// 
+//
+// 你可以按 任意顺序 返回答案 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,0,-1,0,-2,2], target = 0
+//输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [2,2,2,2,2], target = 8
+//输出：[[2,2,2,2]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 200 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 
+//
+// Related Topics 数组 双指针 排序 👍 2052 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @author: Zc
+ * @description: 四数之和可以简化为遍历数组并寻找以当前位置为 target 的三数之和，三数之和则可以变为以当前位置后续值为 target 的两数之和；
+ *               而两数之和还需去重，则可以利用头尾指针搜索当前位置值的所有可能，搜索完毕当前值后跳过重复值即可，不断遍历数组直到末尾。
+ * @date: 2025/4/20 0:06
+ * @param null
+ * @return
+ */
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 3; i++) {
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    double sum = 0.0 + nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < nums.length - 1 && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (right > j && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+                while (j < nums.length - 2 && nums[j] == nums[j + 1]) {
+                    j++;
+                }
+            }
+            while (i < nums.length - 3 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
