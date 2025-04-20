418892aab3d02b5de663487f27cb57597aca25c8
zhourongchang399 <497447111@qq.com>
2025-04-20 14:09:16 +0800

    反转字符串

---
+++ src/leetcode/editor/cn/[344]反转字符串.java
@@ -0,0 +1,52 @@
+//编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。 
+//
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = ["h","e","l","l","o"]
+//输出：["o","l","l","e","h"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = ["H","a","n","n","a","h"]
+//输出：["h","a","n","n","a","H"] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁵ 
+// s[i] 都是 ASCII 码表中的可打印字符 
+// 
+//
+// Related Topics 双指针 字符串 👍 953 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @author: Zc
+ * @description: 利用头尾指针，调换对应值，奇数则到中间退出，偶数则 left > right 时退出。
+ * @date: 2025/4/20 14:08
+ * @param null
+ * @return
+ */
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
