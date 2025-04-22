c0953961d1cc7fe79536997480942a4417712dce
zhourongchang399 <497447111@qq.com>
2025-04-22 22:26:58 +0800

    二叉树的层平均值

---
+++ src/leetcode/editor/cn/[637]二叉树的层平均值.java
@@ -0,0 +1,90 @@
+//给定一个非空二叉树的根节点
+// root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10⁻⁵ 以内的答案可以被接受。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[3.00000,14.50000,11.00000]
+//解释：第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+//因此返回 [3, 14.5, 11] 。
+// 
+//
+// 示例 2: 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,15,7]
+//输出：[3.00000,14.50000,11.00000]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 
+//
+// 
+// 树中节点数量在 [1, 10⁴] 范围内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 528 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * @author: Zc
+ * @description: 队列实现层序遍历，每层求平局值并记录。
+ * @date: 2025/4/22 22:26
+ * @param null
+ * @return
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        if(root==null) return new ArrayList<Double>();
+        List<Double> res = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.pollFirst();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.addLast(node.left);
+                }
+                if (node.right != null) {
+                    queue.addLast(node.right);
+                }
+                if (i == size - 1) {
+                    res.add(sum / (i + 1.0));
+                }
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
