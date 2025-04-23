8f1dc540c0bfda15990028623dccd00b62d0f5b7
zhourongchang399 <497447111@qq.com>
2025-04-23 23:23:52 +0800

    删除二叉搜索树中的节点

---
+++ src/leetcode/editor/cn/[450]删除二叉搜索树中的节点.java
@@ -0,0 +1,126 @@
+//给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的
+//根节点的引用。 
+//
+// 一般来说，删除节点可分为两个步骤： 
+//
+// 
+// 首先找到需要删除的节点； 
+// 如果找到了，删除它。 
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入：root = [5,3,6,2,4,null,7], key = 3
+//输出：[5,4,6,2,null,null,7]
+//解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+//一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+//另一个正确答案是 [5,2,6,null,4,null,7]。
+//
+//
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: root = [5,3,6,2,4,null,7], key = 0
+//输出: [5,3,6,2,4,null,7]
+//解释: 二叉树不包含值为 0 的节点
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: root = [], key = 0
+//输出: [] 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 节点数的范围 [0, 10⁴]. 
+// -10⁵ <= Node.val <= 10⁵ 
+// 节点值唯一 
+// root 是合法的二叉搜索树 
+// -10⁵ <= key <= 10⁵ 
+// 
+//
+// 
+//
+// 进阶： 要求算法时间复杂度为 O(h)，h 为树的高度。 
+//
+// Related Topics 树 二叉搜索树 二叉树 👍 1425 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * @author: Zc
+ * @description: 递归遍历搜索需要删除的节点，找不到则返回 root，找到则重新构造当前节点的二叉搜索树，并返回。
+ * @date: 2025/4/23 23:22
+ * @param null
+ * @return
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+        if(root.val == key) {
+            if (root.left == null && root.right == null) {
+                return null;
+            } else if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode temp = root.right.left;
+                root.right.left = root.left;
+                if (temp != null) {
+                    insertNode(root.left, temp);
+                }
+                return root.right;
+            }
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+
+    private void insertNode(TreeNode root, TreeNode temp) {
+        if (root.val >= temp.val) {
+            if (root.left == null) {
+                root.left = temp;
+                return;
+            }
+            insertNode(root.left, temp);
+        } else {
+            if (root.right == null) {
+                root.right = temp;
+                return;
+            }
+            insertNode(root.right, temp);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
