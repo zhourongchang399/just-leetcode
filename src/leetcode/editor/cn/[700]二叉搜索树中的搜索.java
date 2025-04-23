73e7bb4606c72e87b4aaf07ac8ab4ed577991551
zhourongchang399 <497447111@qq.com>
2025-04-23 21:32:19 +0800

    二叉搜索树中的搜索

---
+++ src/leetcode/editor/cn/[700]二叉搜索树中的搜索.java
@@ -0,0 +1,73 @@
+//给定二叉搜索树（BST）的根节点
+// root 和一个整数值
+// val。 
+//
+// 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回
+// null 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+// 
+//
+// 
+//输入：root = [4,2,7,1,3], val = 2
+//输出：[2,1,3]
+// 
+//
+// 示例 2: 
+// 
+// 
+//输入：root = [4,2,7,1,3], val = 5
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数在 [1, 5000] 范围内 
+// 1 <= Node.val <= 10⁷ 
+// root 是二叉搜索树 
+// 1 <= val <= 10⁷ 
+// 
+//
+// Related Topics 树 二叉搜索树 二叉树 👍 501 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * @author: Zc
+ * @description: 递归实现遍历，判断当前节点是否等于 val，是则返回，不是则根据大小搜索左右子树。
+ * @date: 2025/4/23 21:31
+ * @param null
+ * @return
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return null;
+        if(root.val == val) return root;
+        if(root.val > val) return searchBST(root.left, val);
+        return searchBST(root.right, val);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
