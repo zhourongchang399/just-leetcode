96f3cfc6e816168ab44a51230d22730a17a67566
zhourongchang399 <497447111@qq.com>
2025-04-23 17:13:02 +0800

    路径总和

---
+++ src/leetcode/editor/cn/[513]找树左下角的值.java
@@ -0,0 +1,77 @@
+//给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。 
+//
+// 假设二叉树中至少有一个节点。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: root = [2,1,3]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 
+//
+// 
+//输入: [1,2,3,4,null,5,6,null,null,7]
+//输出: 7
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [1,10⁴] 
+// 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 627 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.pollFirst();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.addLast(node.left);
+                }
+                if (node.right != null) {
+                    queue.addLast(node.right);
+                }
+            }
+            ans.add(list);
+        }
+        return ans.get(ans.size() - 1).get(0);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
